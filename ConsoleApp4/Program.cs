577522494bb0b28ecbf130305a501dd129483ef0
calebe <calebe.fazzani.facul@gmail.com>
2020-03-04 11:47:49 -0300

    RESOLUÇÃO EX1

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] nomes =
+			{
+				"Ana",
+				"Damaris",
+				"Alyson",
+				"Fernanda toxica",
+				"Fellipe Necrosado",
+				"Angelozi Vive Menos",
+				"Lari",
+				"Mari"
+			};
+			for(int i = 0; i < nomes.Length; i++)
+			{
+				Console.WriteLine(nomes[i]);
+			}
+			Console.ReadKey();
+		}
+	}
+}
